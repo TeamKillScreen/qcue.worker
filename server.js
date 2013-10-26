8fafaecee713ef58a190e03c35208d7cf07d00df
Richard Pearson <github@catdevnull.co.uk>
2013-10-26 19:26:34 +0100

    Now monitors for automated queues and can process them

--- server.js
+++ server.js
@@ -1,12 +1,16 @@
+require('log-timestamp');
+
 var http = require('https');
 var config = require('./config');
 var Firebase = require('firebase');
 
-var tasksRef = new Firebase(config.FirebaseURL);
+var tasksRef = new Firebase(config.TasksFirebaseURL);
+var queuesRef = new Firebase(config.QueuesFirebaseURL);
+
+var managedQueues = [];
 
 function SendSMS(number, message)
 {
-	//The url we want is: 'www.random.org/integers/?num=1&min=1&max=10&col=1&base=10&format=plain&rnd=new'
 	var options = {
 	  host: 'api.clockworksms.com',
 	  path: '/http/send.aspx?key=' + config.ClockworkSMSKey + '&to=' + number + '&content=' + message
@@ -17,17 +21,19 @@ function SendSMS(number, message)
 
 	  //another chunk of data has been recieved, so append it to `str`
 	  response.on('data', function (chunk) {
-	    str += chunk;
+		str += chunk;
 	  });
 
 	  //the whole response has been recieved, so we just print it out here
 	  response.on('end', function () {
-	    console.log(str);
+		console.log(str);
 	  });
 	}
 
 	http.request(options, callback).end();
 }
+
+console.log("Worker initialising")
 
 tasksRef.on('child_added', function(snapshot) {
   //console.log(snapshot)
@@ -37,20 +43,61 @@ tasksRef.on('child_added', function(snapshot) {
   var processed = item.processed, task = item.task, payload = item.payload;
   if(!processed)
   {
-  	switch(task)
+	switch(task)
 	{
-	  	case 'sms':
-	  		console.log('Received SMS task. Number: ' + payload.number);
-	  		SendSMS(payload.number, payload.message)
-	  		break;
-	  	default:
-	  		console.log('Unknown task: ' + task);
-	  		break;
+		case 'sms':
+			console.log('Received SMS task. Number: ' + payload.mobile);
+			SendSMS(payload.mobile, payload.message)
+			break;
+		default:
+			console.log('Unknown task: ' + task);
+			break;
 
-	  	
+		
 	}
 
 	snapshot.ref().remove();
   }
   
 });
+
+queuesRef.on('child_added', function(snapshot) {
+	item = snapshot.val();
+
+	if(item.type == 'automated')
+	{
+		console.log('Found automated queue ' + item.name + '. Monitoring')
+
+		managedQueues.push({name: item.name, queue: snapshot.ref(), timer: item.gap, lastChecked: new Date()});
+	}
+});
+
+console.log("Worker initialised");
+
+setInterval(function() {
+	//console.log("Checking automated queues")
+
+	var length = managedQueues.length,
+		element = null;
+	for (var i = 0; i < length; i++)
+	{
+		element = managedQueues[i];
+	  
+		//console.log(element);
+
+		var now = new Date();
+		var diffTime = now.getTime() - element.lastChecked.getTime();
+
+		//console.log(diffTime);
+
+		if(diffTime > element.timer * 1000)
+		{
+			console.log("Processing queue: " + element.name);
+
+			//DO STUFF
+
+			element.lastChecked = now;
+		}
+	}
+}, 1000)
+
